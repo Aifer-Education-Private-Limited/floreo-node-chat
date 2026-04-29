a5c4e65ff2973733417a1116aab42d6d322fe85e
akilsalamnkaifer <akilsalamnkaifer@gmail.com>
2026-04-29 17:42:27 +0530

    client or therapy socket created

--- server.js
+++ server.js
@@ -130,4 +130,72 @@ io.on("connection", (socket) => {
   });
 });
 
+// ======================
+// /clientOrTherapy NAMESPACE
+// ======================
+const clientOrTherapyIO = io.of("/clientOrTherapy");
+
+clientOrTherapyIO.on("connection", (socket) => {
+  console.log("clientOrTherapy Connected:", socket.id);
+
+  // ======================
+  // JOIN PRIVATE CHAT
+  // ======================
+  socket.on("joinChat", async (d) => {
+    
+    const parsed = typeof d === "string" ? JSON.parse(d) : d;
+    const users = parsed?.users;
+    const subject = parsed?.subject;
+    
+    const res = await fetch(`${BASE_URL}/api/method/floreo.api.client_app.chat.join_chat`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ users, subject }),
+    });
+
+    const data = await res.json();
+    
+    const chatId = data.chat_id;
+
+    socket.join(chatId);
+    socket.emit("chatJoined", { chatId });
+
+    const msgRes = await fetch(`${BASE_URL}/api/method/floreo.api.client_app.chat.get_messages`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ chat_id: chatId }),
+    });
+
+    const msgData = await msgRes.json();
+
+    (msgData.message?.messages || msgData.messages).forEach((m) => {
+      socket.emit("OneByOnemessage", m);
+    });
+  });
+
+  // ======================
+  // SEND PRIVATE MESSAGE
+  // ======================
+  socket.on("Sendmessage", async (msg) => {
+    const parsed = typeof msg === "string" ? JSON.parse(msg) : msg;
+    const message = parsed?.message;
+    const sourceId = parsed?.sourceId;
+    const chatId = parsed?.chatId;
+    const isImage = parsed?.isImage;
+    const isVoice = parsed?.isVoice;
+    const isPDF = parsed?.isPDF;
+
+  const res = await fetch(`${BASE_URL}/api/method/floreo.api.client_app.chat.send_message`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body :JSON.stringify({ message, sourceId, chatId, isImage, isVoice, isPDF }),
+  });
+
+  const data = await res.json();
+
+  clientOrTherapyIO.to(msg.chatId).emit("OneByOnemessage", data.message);
+});
+
+});
+
 server.listen(5000, "0.0.0.0", () => console.log("Server running"));
